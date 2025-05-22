524df2f69e0f0b304ebea5184219905f48a6b311
Kanavbhalla89 <kanavbhallaa@gmail.com>
2025-05-22 13:49:13 +0530

    Initial commit

---
+++ frontend/src/config/restaurant-options-config.ts
@@ -0,0 +1,39 @@
+export const cuisineList = [
+  "Punjabi",
+  "Mughlai",
+  "Rajasthani",
+  " South Indian",
+  "Bengali",
+  "Gujarati",
+  "Maharashtrian",
+  "Kashmiri",
+  "Goan",
+  "American",
+  
+  
+  "Burgers",
+  
+  "Chinese",
+  "Desserts",
+  "French",
+  
+  "Healthy",
+  
+  "Italian",
+  "Japanese",
+  "Korean",
+  "Mexican",
+  "Noodles",
+  ,
+  "Pasta",
+  "Pizza",
+  "Salads",
+  
+  
+  "Sushi",
+  "Tacos",
+  
+  "Thai",
+  
+  "Vegan",
+];
